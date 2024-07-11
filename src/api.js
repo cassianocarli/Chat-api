2ef84f284e058e349b0bbf95ae1e238837caafec
cassiano <cassiano-carli@educar.rs.gov.br>
2024-07-11 17:01:26 -0300

    Finalizando chat parte-2

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extend:true}));
@@ -6,16 +6,22 @@ app.use(express.json());
 
 const router = express.Router();
 
-app.use('/', router.get('/', (req, res, next) =>{
+app.use('/', router.get('/', async(req, res) =>{
     res.status(200).send("<h1>API_CHAT</h1>")
 }));
 
-app.use("/",router.get("/sobre", (req,res,next) =>{
+app.use("/",router.get("/sobre", async(req,res) =>{
     res.status(200).send({
         "nome":"API-CHAT",
         "versão":"0.1.0",
         "autor":"cassiano"
     })
+}));
+
+app.use("/",router.get("/salas", async (req,res) =>{
+    const salaController = require("./controllers/salaController");
+    const resp = await salaController.get();
+    res.status(200).send(resp);
 }));
 
 module.exports = app;
